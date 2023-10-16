5ef6c15735e8ebb51658da4f50614eec5fed1e07
Abdallah <abdallahkorhani1@gmail.com>
2023-10-16 12:29:36 +0300

    Navbar menu integration

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,20 @@
 import "./header.css";
+import { Button, Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
 
 export const Header = () => {
+  const [anchorEl, setanchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  console.log(open);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setanchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setanchorEl(null);
+  };
   return (
     <>
       <div className="container header">
@@ -9,21 +23,55 @@ export const Header = () => {
           <li>
             <a href="#Home">Home</a>
           </li>
-          <li>
-            <a href="#About">About</a>
+          <li onClick={handleClick} className="sections-list">
+            <a href="#">Sections</a>
           </li>
-          <li>
-            <a href="#Academic-Support">Academic Support</a>
-          </li>
-          <li>
-            <a href="#Q&A">Q & A</a>
-          </li>
-          <li>
-            <a href="#Contact-us">Contact us</a>
-          </li>
+          <div className="sections">
+            <li>
+              <a href="#About">About</a>
+            </li>
+            <li>
+              <a href="#Academic-Support">Academic Support</a>
+            </li>
+            <li>
+              <a href="#Q&A">Q & A</a>
+            </li>
+            <li>
+              <a href="#Contact-us">Contact us</a>
+            </li>
+          </div>
+
           <li>
             <a href="#Login">Login</a>
           </li>
+          <Menu
+            id="sections"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            className="sections"
+          >
+            <MenuItem component="li">
+              <a href="#About">About</a>
+            </MenuItem>
+            <MenuItem component="li">
+              <a href="#Academic-Support">Academic Support</a>
+            </MenuItem>
+            <MenuItem component="li">
+              <a href="#Q&A">Q & A</a>
+            </MenuItem>
+            <MenuItem component="li">
+              <a href="#Contact-us">Contact us</a>
+            </MenuItem>
+          </Menu>
         </ul>
       </div>
     </>
