6fd7608a190c83979933a4e737b7b4c3d6588e44
ammar <ammarmalass@gmail.com>
2023-10-17 22:31:16 +0300

    Authorization page has been added

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,7 @@ import "./about.css";
 export const About = () => {
   return (
     <div className="about container" id="About">
-      <h2 className="main-title">About us</h2>
+      <h2 className="section-title">About us</h2>
       <div className="main-content">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsa
         distinctio laudantium debitis qui. Non delectus est quis ipsam
---
+++ src/components/Auth/login/Login.tsx
@@ -0,0 +1,122 @@
+import { activeContext } from "../SharedData.tsx";
+import React, { FormEvent, useState, useContext } from "react";
+import "./login.css";
+
+type prop = {
+  isActive: boolean;
+};
+
+export const Login = ({ isActive }: prop) => {
+  const [email, setEmail] = useState(``);
+  const [password, setPassword] = useState(``);
+  const [validation, setValidation] = useState({
+    validEmail: true,
+    sendData: true,
+    correctData: true,
+  });
+
+  const { setHaveAnAccount } = useContext(activeContext);
+
+  const checkEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    if (/[a-z]\w+@\w+\.\w+/.test(e.target.value) || e.target.value === ``) {
+      setValidation((prev) => {
+        return {
+          ...prev,
+          validEmail: true,
+          sendData: true,
+          correctData: true,
+        };
+      });
+    } else {
+      setValidation((prev) => {
+        return {
+          ...prev,
+          validEmail: false,
+          sendData: true,
+          correctData: true,
+        };
+      });
+    }
+  };
+
+  const checkPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+
+    setValidation((prev) => {
+      return {
+        ...prev,
+        sendData: true,
+        correctData: true,
+      };
+    });
+  };
+
+  const confirmData = (e: FormEvent<HTMLFormElement>) => {
+    if (email === `` || password === ``) {
+      e.preventDefault();
+      setValidation((prev) => {
+        return {
+          ...prev,
+          sendData: false,
+        };
+      });
+    } else {
+      if (!validation.validEmail && !validation.sendData) {
+        // fetch your data here, within this if statement
+        e.preventDefault();
+      } else {
+        e.preventDefault();
+        // post your data here
+      }
+    }
+  };
+
+  return (
+    <form
+      className={`login ${isActive ? `active` : ``}`}
+      onSubmit={confirmData}
+    >
+      <div className="inp">
+        {!validation.validEmail && (
+          <span className="error">*Invalid email</span>
+        )}
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onInput={checkEmail}
+        />
+      </div>
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onInput={checkPassword}
+      />
+      <div className="inp">
+        {!validation.sendData && (
+          <span className="error">*All fields are required</span>
+        )}
+        {!validation.correctData && (
+          <span className="error">*Incorrect email or password</span>
+        )}
+      </div>
+
+      <button className="sign-button">Log in</button>
+      <p className="forgot-password">Forgot you password?</p>
+      <p className="signup-now">
+        Don't have an account?{" "}
+        <span
+          className="back"
+          onClick={() => {
+            setHaveAnAccount(false);
+          }}
+        >
+          Sign up
+        </span>{" "}
+        here.
+      </p>
+    </form>
+  );
+};
